850fdfeb664822d87a51ea6d65b571bc786a453d
RitaBB <ritaballesterosba@gmail.com>
2024-09-11 21:37:10 -0500

    creacion_area_ocupacion

---
+++ controllers/area_ocupacion.js
@@ -0,0 +1,50 @@
+import AreaOcupacion from '../models/area_ocupacion.js';
+
+// Método para crear un área de ocupación
+
+export const crearOcupacion =async (req, res)=> {
+
+    try {
+      const params = req.body;
+
+      // Verificar que llegue desde el body el parámetro text con su información
+      if (!params.text) {
+        return res.status(400).send({
+          status: "error",
+          message: "Debes enviar el nombre de la ocupación",
+        });
+      }
+
+      // Crear el objeto del modelo
+    let newAreaOcupacion = new AreaOcupacion(params);
+
+    // Guardar la nueva publicación en la BD
+    const ocupacionStored = await newAreaOcupacion.save();
+
+    // Verificar si se guardó la publicación en la BD (si existe publicationStored)
+    if (!ocupacionStored){
+        return res.status(500).send({
+          status: "error",
+          message: "No se ha guardado la ocupación"
+        });
+      }
+  
+      // Devolver respuesta exitosa 
+      return res.status(200).send({
+        status: "success",
+        message: "¡Ocupación creada con éxito!",
+        ocupacionStored
+      });
+
+
+
+    } catch (error) {
+
+        return res.status(500).send({
+            status: "error",
+            message: "Error al crear la ocupación"
+          });    
+    }
+
+
+}
--- routes/user_register.js
+++ routes/user_register.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 const router = Router();
 import { registerUser, login } from '../controllers/user_register.js';
+import { crearOcupacion } from "../controllers/area_ocupacion.js";
 
 
 // Ruta para registrar un usuario
 router.post('/register', registerUser);
 router.post('/login', login);
+router.post('/area-ocupacion', crearOcupacion);
 
 // Exportar el Router
 export default router;
